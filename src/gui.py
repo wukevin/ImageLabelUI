76346553a99343f34bd16fd2af71a12f8f3e4b81
Kevin Wu <crazyviolindude@gmail.com>
2020-06-15 11:52:46 -0700

    Make each stroke create closed polygon

--- src/gui.py
+++ src/gui.py
@@ -2,7 +2,8 @@
 Classes for GUI elements
 
 Useful references:
-https://stackoverflow.com/questions/40604233/draw-on-python-tkinter-canvas-using-mouse-and-obtain-points-to-a-list
+- https://stackoverflow.com/questions/40604233/draw-on-python-tkinter-canvas-using-mouse-and-obtain-points-to-a-list
+- https://stackoverflow.com/questions/30168896/tkinter-draw-one-pixel
 """
 
 import tkinter as tk
@@ -17,14 +18,28 @@ class ImageLabeller(tk.Tk):
         self.canvas.pack(side="top", fill="both", expand=True)
 
         self.canvas.bind("<B1-Motion>", self.paintbrush)
+        self.canvas.bind("<Button-1>", self.initialize_paintbrush)
+        self.canvas.bind("<ButtonRelease-1>", self.close_paintbrush)
+
+    def initialize_paintbrush(self, event):
+        """Initialize paintbrush on click"""
+        self.recorded_points.append([(event.x, event.y)])
 
     def paintbrush(self, event):
+        """Draw paintbrush line"""
         pos = event.x, event.y
-        self.recorded_points.append(pos)
-        if len(self.recorded_points) > 1:
-            self.canvas.create_line(
-                *self.recorded_points[-2], *self.recorded_points[-1]
-            )
+        self.recorded_points[-1].append(pos)
+        self.canvas.create_line(
+            *self.recorded_points[-1][-2], *self.recorded_points[-1][-1]
+        )
+
+    def close_paintbrush(self, event):
+        """Close the paintbrush shape on click release"""
+        pos = event.x, event.y
+        self.recorded_points[-1].append(pos)
+        self.canvas.create_line(
+            *self.recorded_points[-1][-1], *self.recorded_points[-1][0]
+        )
 
     def clearall(self):
         self.canvas.delete("all")
